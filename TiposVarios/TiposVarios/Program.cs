92891585f1c3208ad138aa4643f341062682d92e
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-02-22 10:11:50 +0000

    adicionado todos os ficheiros modificados

---
+++ TiposVarios/TiposVarios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string a = @"/c/Users/pedro/source/repos";
+            string b = "  \u0298";
+            Console.WriteLine(" {0}{1} ", a, b);
+
+
+
+
+        }
+    }
+}
