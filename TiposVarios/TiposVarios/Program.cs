b668ca65c094344400a7441593739d6742588acc
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-02-22 12:33:12 +0000

    adicionado todos os ficheiro Cilindro

--- TiposVarios/TiposVarios/Program.cs
+++ TiposVarios/TiposVarios/Program.cs
@@ -11,10 +11,12 @@ namespace TiposVarios
         static void Main(string[] args)
         {
             Console.Clear();
-            string a = @"/c/Users/pedro/source/repos";
-            string b = "  \u0298";
-            Console.WriteLine(" {0}{1} ", a, b);
-
+            int x = 9, y = 2;
+            string a = @"PWD: /c/Users/pedro/source/repos  ";
+            string b = "  \u02e5";
+            string c = $"x = {x} e y = {y}  x + y = {x + y} ";
+            Console.WriteLine( "{0}.{1}", x, y);
+            Console.WriteLine(format :" {0} ||| {1}  ||| {2} \n", arg0: a, arg1: b, arg2: c);
 
 
 
